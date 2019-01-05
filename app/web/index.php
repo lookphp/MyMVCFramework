59a0b0b9f5ca2a401b697561fecfc8d7c0021d6c
刘兴旺 <liuxingwang@syberos.com>
2019-01-05 15:49:55 +0800

    增加路由，添加重写
    [提交原因]
    [详细描述]
    [Bug ID]
    [Bug 原因]
    [解决方法]
    [其它]

--- app/web/index.php
+++ app/web/index.php
@@ -1,2 +1,20 @@
 <?php
-phpinfo();
+
+//todo 判断PHP的版本是否合法
+
+//todo 初始化系统市区
+
+//加载路由
+require '../../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/',function(){
+    echo 'hello';
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello, " . $args['name']);
+});
+
+$app->run();
